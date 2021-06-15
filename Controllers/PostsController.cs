1910d71a8e50c0654d41082d3c8a6c205e0277a3
johnvillar41 <johndanielvillar123@gmail.com>
2021-06-15 09:33:30 +0800

    Created new Post COntroller

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Controllers
+{
+    public class PostsController : Controller
+    {
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+    }
+}
